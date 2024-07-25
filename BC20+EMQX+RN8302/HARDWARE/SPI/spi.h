5fa60fd7af96366bb156639aacf5a82c5f39575f
Programmer <61234219+bald-prince@users.noreply.github.com>
2024-07-25 13:24:29 +0800

    Delete BC20+EMQX+RN8302 directory

--- BC20+EMQX+RN8302/HARDWARE/SPI/spi.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SPI_H_
-#define _SPI_H_
-
-#include <stdio.h>
-#include "stm32f10x.h"
-#include "config.h"
-
-
-#endif
--- BC20+EMQX+RN8302/HARDWARE/STRHEX/HEXSTR.h
+++
@@ -1,6 +0,0 @@
-//#include "sys.h"
-#include "string.h"    
-
-
-extern char Value2Hex(const int value);
-extern int Str2Hex(char *str,char *hex);
